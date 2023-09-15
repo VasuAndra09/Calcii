e686aaccae1a448c5c38f968e74df06eba403f23
VasuAndra09 <vasuandra2001@gmail.com>
2023-09-15 14:41:29 +0530

    new application

---
+++ Script.js
@@ -0,0 +1,27 @@
+function add() {
+  let a = inp1.value;
+  let b = inp2.value;
+  let sum = a + b;
+  document.getElementById("out").innerHTML = sum;
+}
+function sub() {
+  let a = inp1.value;
+  let b = inp2.value;
+  let sub = a - b;
+  document.getElementById("out").innerHTML = sub;
+}
+function multi() {
+  let a = inp1.value;
+  let b = inp2.value;
+  let multi = a * b;
+  document.getElementById("out").innerHTML = multi;
+}
+function div() {
+  let a = inp1.value;
+  let b = inp2.value;
+  let div = a / b;
+  document.getElementById("out").innerHTML = div;
+}
+function pict() {
+  document.getElementById("pic").style.display = "block";
+}
